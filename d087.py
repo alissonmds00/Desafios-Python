7fa9f50e5f536255f1c156558cb3ec6aaa8bf20a
Alisson Matias <alissonmds00@gmail.com>
2022-06-03 16:13:27 -0300

    fix: consertado o desafio 88 - matriz melhorada

--- d087.py
+++ d087.py
@@ -1,7 +1,6 @@
 matriz = [[], [], []]
 pares = 0
 coluna3 = 0
-maior = 0
 for linha in range(3):
     for coluna in range(3):
         n = int(input(f'Digite o valor que será inserido em [{linha+1}], [{coluna+1}]: '))
@@ -11,14 +10,12 @@ for linha in range(3):
 for linha in range(3):
     for coluna in range(3):
         print(f'[{matriz[linha][coluna]:^5}]', end=' ')
-        coluna3 += (matriz[linha][2])
-        if coluna == 2 and linha == 0:
-            maior = matriz[0][2]
-        elif coluna == 2 and matriz[linha][2] > maior:
-            maior = matriz[linha][2]
+        if coluna == 2:
+            coluna3 += matriz[linha][2]
     print()
+maior = max(matriz[1])
 print('-=-' * 30)
 print(f'A soma dos valores pares é igual a {pares}')
-print(f'A soma dos valores da terceira coluna  {coluna3}')
-print(f'O maior valor da linha 2 é {maior}')
+print(f'A soma dos valores da terceira coluna é: \033[31m{coluna3}\033[m')
+print(f'O maior valor da linha 2 é: \033[36m{maior}\033[m')
  
