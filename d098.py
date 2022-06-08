398711e230a7c3f872297fec65801868664c6207
Alisson Matias <alissonmds00@gmail.com>
2022-06-08 00:31:52 -0300

    feat: adicionado até o desafio 98

--- d098.py
+++ d098.py
@@ -1,16 +1,29 @@
 def contador():
+    from math import fabs as mod
+    from time import sleep
     i = int(input('Informe qual será o ponto de partida de contagem: '))
     f = int(input('Informe qual será o ponto final da contagem: '))
     p = int(input('Informe o intervalo entre os números de contagem: '))
+    if p == 0:
+        p = 1
     print('-=-' * f)
     for c in range(1, 11):
         print(f'{c}', end=' ➯ ')
+        sleep(0.1)
     print('fim')
-    for d in range(10, -1, -1):
+    for d in range(10, -1, -2):
         print(f'{d}', end=' ➯ ')
+        sleep(0.1)
     print('fim')
-    for e in range(i, f+1, p):
-        print(f'{e}', end=' ➯ ')
+    if i > f:
+        for cont in range(i, f-1, -int(mod(p))):
+            print(f'{cont}', end=' ➯ ')
+            sleep(0.1)
+    print('fim')
+    if i < f:
+        for cont in range(i, f+1, p):
+            print(f'{cont}', end=' ➯ ')
+            sleep(0.25)
     print('fim')
     print('-=-' * f)
 
