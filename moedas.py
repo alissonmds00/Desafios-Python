2b0c78f014403c8cea753cca91633220746ddcd4
Alisson Matias <alissonmds00@gmail.com>
2022-07-11 01:07:27 -0300

    adicionados o desafio 109 completo

--- moedas.py
+++ moedas.py
@@ -1,27 +1,27 @@
-def aumentar(x=0, v=0):
+def aumentar(x=0, v=0, format=False):
     """
     Digite quantos por cento deseja aumentar
      separado por vírgula do valor
     """
     percentual = x / 100
     vf = (percentual * v) + v
-    return vf
+    return vf if format is False else (moeda(vf))
 
 
-def reduzir(x=0, v=0):
+def reduzir(x=0, v=0, format=False):
     percentual = x / 100
     vf =  v - (percentual * v)
-    return vf
+    return vf if format is False else (moeda(vf))
 
 
-def metade(x=0):
+def metade(x=0, format=False):
     m = x/2
-    return m
+    return m if format is False else (moeda(m))
 
 
-def dobro(x=0):
+def dobro(x=0, Format=False):
     d = x*2
-    return d
+    return d if format is False else (moeda(d))
 
 
 def moeda(v = 0, u = 'R$'):
